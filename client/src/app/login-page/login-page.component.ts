a9e5b6693e5a69d5d3b62c47043bf72b89597a39
i.maryniuk <i.maryniuk@itprosteer.com>
2021-09-22 18:09:59 +0300

    start client part

---
+++ client/src/app/login-page/login-page.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+
+export class LoginPageComponent implements OnInit {
+
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSubmit() {
+
+  }
+
+}
